af790e9a553e892b1c53d162405dde2f6c4b89cb
Tanmay Jain <jaintanmay103@gmail.com>
2026-06-05 18:39:09 +0530

    Day 1: Hello World and variables

---
+++ day01.py
@@ -0,0 +1,29 @@
+name="Tanmay Jain"
+city = "Delhi"
+course = "Python"
+dream = "To be a founder"
+hours_per_day = "3"
+
+print("Hello my name is",name)
+print("I live in",city)
+print("I am learning",course)
+
+age = 18 
+year = 2026
+days_to_goal = 180
+print("Age:",age)
+print("Days left to become developer:",days_to_goal)
+
+height = 5.9
+print("Height:",height)
+
+is_student = True
+loves_coding = True
+print("Am I a student:",is_student)
+print("Do I love coding:",loves_coding)
+
+print(f"My name is {name} and I am {age}years old.")
+print(f"I live in {city}. I have {days_to_goal} days to reach my goal.")
+print(f"Student: {is_student} I love coding:{loves_coding}")
+
+
