07ffe89a7c186ca71f7cc84b057a07f3bf873070
Tanmay Jain <jaintanmay103@gmail.com>
2026-06-06 17:10:14 +0530

    Day 3 : Operators

---
+++ day03.py
@@ -0,0 +1,63 @@
+age = 19 
+print("Age:",age)
+print("Type:",type(age))
+
+height = 178.5
+print("Height:",height)
+print("Type:",type(height))
+
+number = 1+2j
+print("Number:",number)
+print("Type:",type(number))
+
+base = int(input("Enter base:"))
+height = int(input("Enter height:"))
+area = 0.5 * base * height
+print("Area of triangel:",area)
+
+side_a = int(input("Enter side a :"))
+side_b = int(input("Enter side b :"))
+side_c  =int(input("Enter side c :"))
+perimeter = side_a + side_b + side_c
+print("Perimeter of triangle :",perimeter)
+
+#Equation Form: y = mx + b
+#Given Equation: y = 2x - 2
+m = 2
+b = -2
+y_intercept = b
+x_intercept = -b / m
+print("Slpoe :",m)
+print("Y-intercept :",y_intercept)
+print("X-intercept :",x_intercept)
+
+print(len("python") > len("dragon"))
+
+print("on in python" and "on in dragon")
+
+sentence = "I hope this corse is not full of jagron"
+print("jagron" in sentence)
+
+print("on" not in "python")
+print("on" not in "dragon")
+
+text = "python"
+length_val = len(text)
+float_val = float(length_val)
+string_val = str(float_val)
+print("The final value is :",string_val)
+
+number = 8
+is_even = number % 2 == 0
+print("Is number even?:",is_even)
+
+floor_div = 7 // 3
+int_val = int(2.7)
+are_equal = (floor_div == int_val)
+print("Are eqaul ?:",are_equal)
+
+type1 = '10'
+type2 = 10
+are_equal = (type1 == type2)
+print("Are equal ?:",are_equal)
+
